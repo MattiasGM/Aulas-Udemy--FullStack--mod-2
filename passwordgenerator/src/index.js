ac8f4765be9dcfc6267ca2eca9cbaa0c492b02fb
Mattias Gaspar <mattiasmac2000@gmail.com>
2022-09-28 23:21:26 -0300

    update

--- passwordgenerator/src/index.js
+++ passwordgenerator/src/index.js
@@ -1,10 +1,49 @@
 import GeraPassword from "./modules/geraPassword.js";
+import Creat from "./modules/creatTags.js"
+
+const { creat } = new Creat()
 
 import './style/index.css'
 
 (function app() {
     const newPassword = new GeraPassword()
-    const generaterCPF = document.querySelector('.container')
 
-    generaterCPF.innerHTML = `CPF:  ${newPassword.generatePassword()}`
+    const container = document.querySelector('.container')
+
+    const panel = creat('div', 'divPanel')
+    panel.innerHTML = `Password:  <p>${newPassword.generatePassword(15, true, true, true, true)}</p>`
+    container.appendChild(panel)
+
+    const numbersOfCharacters = mount(container, 'text', 'numbersOfCharacters', 'inputNumbers', 'Quantidade de caracteres')
+    const addNumbers = mount(container, 'checkbox', 'addNumbers', 'checkbox', 'Adicionar números')
+    const upperCase = mount(container, 'checkbox', 'upperCase', 'checkbox', 'Letras maiúsculas')
+    const lowerCase = mount(container, 'checkbox', 'lowerCase', 'checkbox', 'Letras minúsculas')
+    const symbols = mount(container, 'checkbox', 'symbols', 'checkbox', 'Simbolos')
+
+    const generater = creat('input', 'submit', 'button', 'Gerar Senha', 'button')
+    container.appendChild(generater)
+
+    document.addEventListener('click', e => {
+        if(e.target.id === 'submit') {
+            const numbersOfCharacters = document.querySelector('#numbersOfCharacters')
+            const addNumbers = document.querySelector('#addNumbers')
+            const upperCase = document.querySelector('#upperCase')
+            const lowerCase = document.querySelector('#lowerCase')
+            const symbol = document.querySelector('#symbol')
+
+            
+        }
+    })
+
 })()
+
+function mount(container, input, id, className, text) {
+    const p = creat('p');
+    const Input = creat('input', id, className, '', input, '0');
+    const Label = creat('label');
+    p.innerHTML = text;
+    Label.appendChild(p);
+    Label.appendChild(Input);
+    container.appendChild(Label)
+    return;
+}
--- passwordgenerator/src/modules/geraPassword.js
+++ passwordgenerator/src/modules/geraPassword.js
@@ -1,22 +1,54 @@
 export default class GeraPassword {
-    rand(min = 100000000, max = 999999999) {
-        return String(Math.floor(Math.random() * (max - min) + min))
+
+    randPass(length, number, upperCase, lowerCase, symbols) {
+        let password = ''
+        let index = 0
+
+        while(index < length) {
+            if(!number && !upperCase && !lowerCase && !symbols) return password = INVÁLIDO
+            if(upperCase) {
+                password += this.creatDigit().toUpperCase()
+                index++
+                if(index >= length) break
+            }
+            if(lowerCase) {
+                password += this.creatDigit().toLowerCase()
+                index++
+                if(index >= length) break
+            }
+            if(number) {
+                password += String(Math.floor(Math.random() * 9))
+                index++
+                if(index >= length) break
+            }
+            if(symbols) {
+                password += this.creatSymbol()
+                index++
+                if(index >= length) break
+            }
+        }
+        return password
     }
 
-    formated() {
-        return(
-            console.log('')
-        )
+    creatSymbol() {
+        let symbol = ['!', '@', '#', '$', '%']
+
+        const rand = Number(Math.floor(Math.random() * 5))
+
+        return symbol[rand]
     }
 
-    generatePassword() {
-        const password = console.log('')
-        const password2 = console.log('')
-        const password3 = console.log('')
-        const password4 = console.log('')
+    creatDigit() {
+        let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-        return this.formated()
+        const rand = Number(Math.floor(Math.random() * 26))
+
+        return alphabet[rand]
+    }
+
+    generatePassword(digitsLength, addNumber, upperCase, lowerCase, symbols) {
+        let password = this.randPass(digitsLength, addNumber, upperCase, lowerCase, symbols)
+
+        return password
     }
 }
-
-// TERMINAR GERADOR DE SENHAS !!!!!!!!!!!
