8509a9f0571281aafb95825b3e7dea9a61754017
Mattias Gaspar <mattiasmac2000@gmail.com>
2022-09-28 23:52:10 -0300

    v

--- passwordgenerator/src/index.js
+++ passwordgenerator/src/index.js
@@ -1,20 +1,20 @@
 import GeraPassword from "./modules/geraPassword.js";
 import Creat from "./modules/creatTags.js"
 
+const newPassword = new GeraPassword()
 const { creat } = new Creat()
 
 import './style/index.css'
 
 (function app() {
-    const newPassword = new GeraPassword()
 
     const container = document.querySelector('.container')
 
     const panel = creat('div', 'divPanel')
-    panel.innerHTML = `Password:  <p>${newPassword.generatePassword(15, true, true, true, true)}</p>`
+    panel.innerHTML = `Password: Selecione as opções`
     container.appendChild(panel)
 
-    const numbersOfCharacters = mount(container, 'text', 'numbersOfCharacters', 'inputNumbers', 'Quantidade de caracteres')
+    const numbersOfCharacters = mount(container, 'number', 'numbersOfCharacters', 'inputNumbers', 'Quantidade de caracteres')
     const addNumbers = mount(container, 'checkbox', 'addNumbers', 'checkbox', 'Adicionar números')
     const upperCase = mount(container, 'checkbox', 'upperCase', 'checkbox', 'Letras maiúsculas')
     const lowerCase = mount(container, 'checkbox', 'lowerCase', 'checkbox', 'Letras minúsculas')
@@ -25,12 +25,16 @@ import './style/index.css'
 
     document.addEventListener('click', e => {
         if(e.target.id === 'submit') {
-            const numbersOfCharacters = document.querySelector('#numbersOfCharacters')
+            const container = document.querySelector('.container')
+            const panel1 = document.querySelector('#divPanel')
+            const numbersOfCharacters = Number(document.querySelector('#numbersOfCharacters').value)
             const addNumbers = document.querySelector('#addNumbers')
             const upperCase = document.querySelector('#upperCase')
             const lowerCase = document.querySelector('#lowerCase')
             const symbol = document.querySelector('#symbol')
 
+            panel1.innerHTML = `Password: <p>${newPassword.generatePassword(numbersOfCharacters, addNumbers.checked, upperCase.checked, lowerCase.checked, symbol.checked)}</p>`
+            container.appendChild(panel)
             
         }
     })
